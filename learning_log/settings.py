1eddf0abb348557aec6419695fbf40f698315f32
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-21 16:11:52 +0100

    Add allowed host

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','learning-log-108k', 'onrender']
+ALLOWED_HOSTS = ['learning-log-108k.onrender.com']
 
 
 # Application definition
